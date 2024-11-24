692b8b73b8b6455dac603990b7916ab00cd14ff5
Shubhamsingh22ns <shubhamsingh22ns@gmail.com>
2024-11-25 00:25:34 +0530

    Remove node_modules from Git tracking and update db.js

--- db.js
+++ db.js
@@ -1,13 +1,30 @@
 const mongoose = require("mongoose");
 
-const URL = "mongodb://localhost:27017/Student";
+// Use your MongoDB connection string here
+const URL = "mongodb+srv://shubhamsingh22ns:JDXwIQFTGw1NnGRz@cluster0.chzv2.mongodb.net/";
 
-mongoose.connect(URL);
+// Establish connection to MongoDB
+mongoose.connect(URL, {
+    useNewUrlParser: true,  // Correct typo here
+    useUnifiedTopology: true  // Keep this for compatibility with the new MongoDB driver
+});
 
+// Access the connection object
 const db = mongoose.connection;
 
-db.on("connection",()=>{
-    console.log("Coneected");
-})
+// Listen for successful connection
+db.on("connected", () => {  // Use 'connected' event instead of 'connection'
+    console.log("Connected to MongoDB");
+});
+
+// Handle any connection errors
+db.on("error", (err) => {
+    console.log("Error connecting to MongoDB:", err);
+});
+
+// Optional: Listen for disconnection events
+db.on("disconnected", () => {
+    console.log("Disconnected from MongoDB");
+});
 
 module.exports = db;
